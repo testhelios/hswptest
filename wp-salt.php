ab78917dc85d799e78e2ded7ba1ad569b7f6b045
testhelios <ankit.suthar.hs@gmail.com>
2016-07-20 19:02:41 +0530

    initial commit

---
+++ wp-salt.php
@@ -0,0 +1,10 @@
+<?php
+define('AUTH_KEY',         '|m$|YQ5}py+B~-=-O$Ju/+/e|q6I+D-JW^+]*;<cZ^s*<waCkSz++6RK3Kjy@Vuw');
+define('SECURE_AUTH_KEY',  '*D-3g_R*Ko&6+g_23=hw)Z[(GHdI4r<X61My?>8:PVKw1EBZOgs^[J$8m)!5nM@D');
+define('LOGGED_IN_KEY',    'GFJ.pP{|% cT9|(>SQD1]!UUY&w?$$#VMR]fipQqDY:CJ43J+f%>3LHn)N145|%-');
+define('NONCE_KEY',        '{DY4?7Om-+GZoTE<0Qat2=H</@,h tpt?1,IX@u2E{ k$=$|dDWdg@hSz2HzFKpP');
+define('AUTH_SALT',        'Mp_?GQr*BiC(VHz.dB4/+Dja`#seJPMf[q,@4oH@=:VOv|:54gZAEJ9N*7@@8z+~');
+define('SECURE_AUTH_SALT', 'Z`CUp><KEj3cILsQ6}=j3m)sJ2G+3bc-sly4r+23#oEuUVV|ogH|8/UjEUPEEC![');
+define('LOGGED_IN_SALT',   '~D:fgz%0:MATEY~P;pteVK1TnYoV4AzjKGI~MZd|f&O%j-?CEtvyY PsLJZ9v]l]');
+define('NONCE_SALT',       '@9|w6Q>X->CF^OftNdD?X%N/@7gb+a_}ov-+E^$LDWlGiNH=y()t;!K:#wz6Ya@T');
+?>
